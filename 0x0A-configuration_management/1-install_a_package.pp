7ee94a431a8f719a1203c0c0f1982badab44bf64
Brian Omondi <omondiibrian00@gmail.com>
2023-09-22 15:48:03 +0300

    used the package resource type

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,20 +1,5 @@
 # Install flask based on set attributes
-# Flask needs py and pip3. Ensure they are already installed
-package {'python3':
-  ensure => installed,
-}
-
-package { 'python3-pip':
-  ensure => installed,
-}
-
-# Attributes for flask installation
-# set env when using package managers - common practice
-exec { 'Flask':
-  command     => 'pip3 install flask==2.1.0',
-  path        => ['/usr/bin', '/bin'],
-  logoutput   => true,
-  refreshonly => true,
-  environment => ['LC_ALL=C'],
-  unless      => 'pip3 show flask',
+package { 'flask':
+  ensure => '2.1.0',
+  provider => 'pip3',
 }
--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,7 +1,8 @@
 # A manifest that executes a pkill command
 exec {'killmenow':
-  command     => 'pkill -f "killmenow"',
-  onlyif      => 'pgep -f "killmenow"',
+  command     => 'pkill killmenow',
+  onlyif      => 'pgep killmenow',
+  provider    => shell,
   path        => ['/usr/bin', '/bin'],
   logoutput   => true,
   refreshonly => true,
