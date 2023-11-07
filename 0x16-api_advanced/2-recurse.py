c1ce3df6cf28ac65a7daeb37fbd5a80024d375de
Brian Omondi <omondiibrian00@gmail.com>
2023-11-08 01:44:30 +0300

    fix on recursive function

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -3,32 +3,27 @@
 import requests
 
 
-def recurse(subreddit, hot_list=[], count=0, next_page=None):
+def recurse(subreddit, hot_list=[], after=None):
     """ A recurcive function to query Reddit API and rerturn a list
     of all hot articles """
     url = 'http://www.reddit.com/r/{}/hot.json'.format(subreddit)
     headers = {'user-agent': 'briansAPI'}
 
-    if next_page:
-        url += '?after={}'.format(next_page)
+    if after:
+        url += '?after={}'.format(after)
 
     try:
         response = requests.get(url, headers, allow_redirects=False)
 
-        if response.status_code != 200:
-            return None
-
-        data = response.json()['data']
-        posts = data['children']
-        for post in posts:
-            count += 1
-            hot_list.append(post['data']['title'])
-
-        next_page = data['after']
-        if next_page is not None:
-            return recurse(subreddit, hot_list, count, next_page)
-        else:
-            return hot_list
+        if response.status_code == 200:
+            data = response.json()
+            if 'data' in data and 'children' in data['data']:
+                for post in data['data']['children']:
+                    hot_list.append(post['data']['title'])
+                if 'after' in data['data']:
+                    return recurse(subreddit, hot_list, data['data']['after'])
+                else:
+                    return hot_list
 
     except ValueError:
         return o
