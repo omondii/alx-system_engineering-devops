b5ff74326a92ad2b45eccee97b9958d7e5cfaa93
Brian Omondi <omondiibrian00@gmail.com>
2023-11-08 00:16:41 +0300

    How many subs

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Api that queries the RedditAPI
+"""
+import requests
+
+def number_of_subscribers(subreddit):
+    """ Queries the reddit api and returns the number of subscribers"""
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'user-agent': 'briansAPI'}
+
+    try:
+        response = requests.get(url, headers, allow_redirects=False)
+        # Check if the request was succesful, else exit with 0
+        if response.status_code != 200:
+            return 0
+        data = response.json()
+        subs = data['data']['subscribers']
+        return subs
+
+    except ValueError:
+        return 0
