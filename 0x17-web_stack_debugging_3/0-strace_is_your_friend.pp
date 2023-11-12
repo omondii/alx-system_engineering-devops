621e938465e7a12b75a7cf3d26dd9ab837d7baf2
Brian Omondi <omondiibrian00@gmail.com>
2023-11-12 22:34:52 +0300

    Fixing file name in wordpress

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,7 @@
+# Fix typo in wordpress settings file
+exec { 'fix wordpres extension':
+     environment => ['DIR=/var/www/html/wp-settings.php',
+     		    'OLD=phpp',
+		    'NEW=php'],
+     command     => 'sudo sed -i "s/$OLD/$NEW/" $DIR',
+     path        => ['/usr/bin', '/bin'],
