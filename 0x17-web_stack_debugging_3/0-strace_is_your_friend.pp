3a01a8995fb51a4acdfd6aa21a756e1cf54bd6ae
Brian Omondi <omondiibrian00@gmail.com>
2023-11-14 07:44:42 +0300

    Puppet lint fix

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,5 @@
 # Fix typo in wordpress settings file
 exec { 'fix wordpres extension':
-     environment => ['DIR=/var/www/html/wp-settings.php',
-     		    'OLD=phpp',
-		    'NEW=php'],
+     environment => ['DIR=/var/www/html/wp-settings.php','OLD=phpp''NEW=php'],
      command     => 'sudo sed -i "s/$OLD/$NEW/" $DIR',
      path        => ['/usr/bin', '/bin'],
