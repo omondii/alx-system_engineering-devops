5ad0318459c8cd018c44f501b88e9dc6fa10edcd
Brian Omondi <omondiibrian00@gmail.com>
2023-11-17 08:13:52 +0300

    user limits fixed

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,7 @@
+exec {'Os confiig for user':
+  environment => ['DIR=/etc/security/limits.conf',
+                  'OLD=hard nofile 5', 'NEW=hard nofile 50000',
+                  'OLD1=soft nofile 4', 'NEW1=soft nofile 40000'],
+  command     => 'sudo sed -i "s/$OLD/$NEW/" $DIR; sudo sed -i "s/$OLD1/$NEW1/" $DIR',
+  path        => ['/usr/bin', '/bin']
+}
