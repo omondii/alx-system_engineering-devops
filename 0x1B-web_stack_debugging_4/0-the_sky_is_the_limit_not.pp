5f95239bdfe85f0e162f39d6389aa1e976ed93f1
Brian Omondi <omondiibrian00@gmail.com>
2023-11-17 07:51:24 +0300

    FIX; Attempt requirement 4

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,6 @@
 # Increasing nginx requests handling using puppet
 exec {'requests_capacity':
-  environment => ['DIR=/etc/default/nginx', 'OLD=15', 'NEW=4096'],
+  environment => ['DIR=/etc/default/nginx', 'OLD=15', 'NEW=15000'],
   command     => "sudo sed -i 's/\$OLD/\$NEW/' \$DIR; sudo service nginx restart",
   path        => ['/usr/bin', '/bin'],
 }
