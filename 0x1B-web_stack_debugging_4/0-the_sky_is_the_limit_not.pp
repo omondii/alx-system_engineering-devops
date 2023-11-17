03071b63718a861fa9166d2cc27ea9fac478ae16
Brian Omondi <omondiibrian00@gmail.com>
2023-11-17 07:47:02 +0300

    DEL; Deleted 3 consecutive exec functions

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,25 +1,6 @@
 # Increasing nginx requests handling using puppet
 exec {'requests_capacity':
   environment => ['DIR=/etc/default/nginx', 'OLD=15', 'NEW=4096'],
-  command     => "sudo sed -i 's/\$OLD/\$NEW/' \$DIR",
+  command     => "sudo sed -i 's/\$OLD/\$NEW/' \$DIR; sudo service nginx restart",
   path        => ['/usr/bin', '/bin'],
 }
-
-exec {'limits_increase_1':
-  environment => ['DIR=/etc/security/limits.conf', 'LIMITS_ENTRY=ngnix\\tsoft\\tnofile\\t30000'],
-  command     => "sed -i -e '\$a\$LIMITS_ENTRY' \$DIR",
-  path        => ['/usr/bin', '/bin'],
-}
-
-exec {'limits_increase_2':
-  environment => ['DIR=/etc/security/limits.conf', 'LIMITS_ENRTY=nginx\\thard\\tnofile\\t50000'],
-  command     => "sed -i -e '\$a\$LIMITS_ENTRY' \$DIR",
-  path        => ['/usr/bin' ,'/bin'],
-}
-
-exec {'worker limits':
-  environment => ['DIR=/etc/nginx/nginx.conf', 'RLIMITS=worker_rlimit_nofile 30000;'],
-  command     => "sed -i -e '4r \$a\$RLIMITS' \$DIR sudo service nginx restart",
-  path        => ['/usr/bin', '/bin']
-}
-
